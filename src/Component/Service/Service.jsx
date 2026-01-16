82ab4bd7e065b6ed898441bee102674310f4973b
Nex-Lume <nexlume.co@gmail.com>
2026-01-17 00:40:04 +0530

    Finally changes done by Nikhil Messa

--- src/Component/Service/Service.jsx
+++ src/Component/Service/Service.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import "./Services.css";
 import { gsap } from "gsap";
 import "./FlowingMenu.css"; // Make sure this file exists
